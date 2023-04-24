6cf5bff0c681636b4f87a4dd91e4b6b71eb5576c
Piter <{ID}+{username}@users.noreply.github.com>
2023-04-24 07:32:56 +0200

    created project

---
+++ src/main/java/pl/szczerbaniak/App.java
@@ -0,0 +1,13 @@
+package pl.szczerbaniak;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
