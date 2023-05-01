135a79c55e1221ce3bab8642f86b1ed67b893d04
Piter <{ID}+{username}@users.noreply.github.com>
2023-05-01 20:35:21 +0200

    finished working on page with visitor list

--- src/main/resources/front/scripts/displayVisitorsList.js
+++ src/main/resources/front/scripts/displayVisitorsList.js
@@ -22,6 +22,26 @@ async function getVisitors() {
 
 async function writeOutVisitors() {
 	const visitors = await getVisitors();
+	const tableBody = document.querySelector("#visitors > tbody");
+
 	console.log(visitors);
+	visitors.forEach((visitor) => {
+		const template = document.querySelector("#row").content.cloneNode(true);
+		template.querySelector(".id").textContent = visitor.id;
+		template.querySelector(".name").textContent = visitor.name;
+		template.querySelector(".secondName").textContent = visitor.secondName;
+		template.querySelector(".surname").textContent = visitor.surname;
+		template.querySelector(".pesel").textContent = visitor.pesel;
+		template.querySelector(".nameOfIdentityDocument").textContent = visitor.nameOfIdentityDocument;
+		template.querySelector(".numberOfIdentityDocument").textContent = visitor.numberOfIdentityDocument;
+		template.querySelector(".email").textContent = visitor.email;
+		template.querySelector(".phoneNumber").textContent = visitor.phoneNumber;
+		template.querySelector(".school").textContent = visitor.school;
+		template.querySelector(".locality").textContent = visitor.locality;
+		template.querySelector(".major").textContent = visitor.major;
+		template.querySelector(".choiceNumber").textContent = visitor.choiceNumber;
+		tableBody.appendChild(template);
+	});
 }
+
 writeOutVisitors();
